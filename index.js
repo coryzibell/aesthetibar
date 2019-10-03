f57f807cb0328de53fc2942a35fcdb9533aa7bb8
Cory Zibell <cory.zibell@digitalsurgeons.com>
2019-10-03 14:35:54 -0400

    Code fixes and readme

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const { menubar } = require('menubar');
 
-console.log(process.cwd() + '/icon.png')
-
 const mb = menubar({
     width: 145,
     height: 35,
-    icon: process.cwd() + '/icon.png'
 });
 
 mb.on('ready', () => {
