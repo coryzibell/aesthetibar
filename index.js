e51a2b930a5a42bcf38e4c7d0a30b68a53fb7245
Cory Zibell <cory.zibell@digitalsurgeons.com>
2019-10-03 11:24:48 -0400

    More updates and icon

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const { menubar } = require('menubar');
 
+console.log(process.cwd() + '/icon.png')
+
 const mb = menubar({
     width: 145,
-    height: 35
+    height: 35,
+    icon: process.cwd() + '/icon.png'
 });
 
 mb.on('ready', () => {
